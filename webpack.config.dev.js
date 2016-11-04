45c48e520b6222f5aa27ad4907d2b048004a88c1
Bibek Subedi <bsubedi26@gmail.com>
2016-11-04 13:30:36 -0400

    adding webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+import path from 'path'
+import webpack from 'webpack';
+
+export default {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
+  output: {
+    path: '/',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: [ 'react-hot', 'babel' ]
+      }
+    ]
+  },
+  resolve: {
+    extentions: [ '', '.js' ]
+  }
+}
